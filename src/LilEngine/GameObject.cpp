4808ca639c72dad9a77b7a02235f46fb0ac4bf62
andrey_kalugin <duyha.beitz@gmail.com>
2026-07-19 00:05:02 +0300

    Fixed GameObject constructor constructing new Engine object

--- src/LilEngine/GameObject.cpp
+++ src/LilEngine/GameObject.cpp
@@ -2,6 +2,6 @@
 #include "LilEngine.hpp"
 
 GameObject::GameObject(Transform transform) 
-: m_id(Lil::Engine().GetWorld().GetNewId()), Transformable(transform)
+: m_id(Lil::GetWorld().GetNewId()), Transformable(transform)
 {
 }
