bf4296d963f94a1f3c709695689772cf7c2656b5
andrey_kalugin <duyha.beitz@gmail.com>
2026-08-07 18:57:27 +0300

    Gen heightmap model with data (for raycast)

--- src/LilEngine/utils/MeshHelper.cpp
+++ src/LilEngine/utils/MeshHelper.cpp
@@ -23,8 +23,21 @@ R3D_Model LoadR3DModelFromMesh(R3D_Mesh mesh) {
 R3D_Model HeightmapModel(Image &image, Vector3 size) {
     Texture2D texture = LoadTextureFromImage(image);
 
-    R3D_Mesh mesh = R3D_GenMeshHeightmap(image, size);
-    R3D_Model model = LoadR3DModelFromMesh(mesh);
+    R3D_Model model = {0};
+    R3D_Mesh mesh = {0};
+    R3D_MeshData data = R3D_GenMeshDataHeightmap(image, size);
+    if (!R3D_IsMeshDataValid(data)) return model;
+
+    BoundingBox aabb = {
+        {-size.x * 0.5f,   0.0f, -size.z * 0.5f},
+        { size.x * 0.5f, size.y,  size.z * 0.5f}
+    };
+
+    mesh = R3D_LoadMesh(R3D_PRIMITIVE_TRIANGLES, data, &aabb);
+    model = LoadR3DModelFromMesh(mesh);
+
+    model.meshData = reinterpret_cast<R3D_MeshData*>(RL_MALLOC(sizeof(R3D_MeshData)));
+    *(model.meshData) = data;
 
     //model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture; // Set map diffuse texture
     return model;
