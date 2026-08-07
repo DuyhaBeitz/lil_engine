df358dbc156f8642c0961cabd4a99f6a07d533dc
andrey_kalugin <duyha.beitz@gmail.com>
2026-08-07 21:59:02 +0300

    Fixed MeshHelper.hpp having old functions

--- include/LilEngine/utils/MeshHelper.hpp
+++ include/LilEngine/utils/MeshHelper.hpp
@@ -4,6 +4,3 @@
 
 R3D_Model LoadR3DModelFromMesh(R3D_Mesh mesh);
 R3D_Model HeightmapModel(Image& image, Vector3 size);
-
-void DrawR3DModelWiresEx(R3D_Model model, Vector3 position, Quaternion rotation, Vector3 scale);
-void DrawR3DModelWires(R3D_Model model, Vector3 position, float scale);
