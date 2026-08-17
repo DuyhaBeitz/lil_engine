f75d22ba1ca74bda51da7164013116a8b8cf11c7
andrey_kalugin <duyha.beitz@gmail.com>
2026-08-17 13:26:12 +0300

    Resources are loaded before the world

--- src/LilEngine/SceneManager.cpp
+++ src/LilEngine/SceneManager.cpp
@@ -5,7 +5,7 @@
 void SceneManager::LoadScene(std::string filename) {
     std::ifstream is(filename);
     ArchiveIn a_in(is);
-    a_in(Lil::World(), Lil::Resources(), Lil::Environment());
+    a_in(Lil::Resources(), Lil::Environment(), Lil::World());
     Lil::World().UpdateActorLayout();
     Lil::Resources().ApplyModelSettings();
 }
@@ -13,5 +13,5 @@ void SceneManager::LoadScene(std::string filename) {
 void SceneManager::SaveScene(std::string filename) {
     std::ofstream os(filename);
     ArchiveOut a_out(os);
-    a_out(Lil::World(), Lil::Resources(), Lil::Environment());
+    a_out(Lil::Resources(), Lil::Environment(), Lil::World());
 }
